18202c8e40bb493463e29db563ffc7524e3bc0d6
MichaelM <mmawson1@student.gsu.edu>
2021-09-07 23:30:47 -0400

    The Code for the Sum 1-100

---
+++ Sum.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+sum = 0
+for x in range(1,101):
+    sum = sum + x
+print ("The sum of 1 to 100 is " + str(sum) + ".")
