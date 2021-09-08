2c663846940edd2017f7aa32a5e3c82237a3a907
MichaelM <mmawson1@student.gsu.edu>
2021-09-07 23:38:10 -0400

    Sum.py but with a Comment

--- Sum.py
+++ Sum.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
+# This code calculates the sum of 1 to 100. It uses a for loop.
 sum = 0
 for x in range(1,101):
     sum = sum + x
